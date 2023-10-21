96d24eba62174d90b7ae76151f90995babf8c023
Lavender Sanders <lavendersanders88@yahoo.com>
2023-10-20 23:13:41 -0500

    3rd commit

--- codingevents/src/main/java/org/launchcode/codingevents/controller/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controller/EventController.java
@@ -3,8 +3,10 @@ package org.launchcode.codingevents.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,19 +15,22 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    @GetMapping
-    public String displayEvents(Model model){
-      List <String> events = new ArrayList<>();
-      events.add("Born Day");
-      events.add("It's a Celebration");
-      events.add("Launchcode");
-      events.add("Almost Christmas");
-      model.addAttribute("events", events);
+    private static List<String> events = new ArrayList<>();
 
+    @GetMapping
+    public String displayAllEvents(Model model){
+      model.addAttribute("events", events);
       return "events/index";
     }
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
     }
+
+    @PostMapping("create")
+    public String processForm(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:/events"; //this takes it back to the display events page
+    }
+
 }
