85fb2ce54908f73aeb27ef30b2dbc5aba4c3c1aa
Lavender Sanders <lavendersanders88@yahoo.com>
2023-10-25 20:01:40 -0500

    DisplayErrorsAdded

--- codingevents/src/main/java/org/launchcode/codingevents/controller/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controller/EventController.java
@@ -1,9 +1,11 @@
 package org.launchcode.codingevents.controller;
 
 
+import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
@@ -24,14 +26,23 @@ public class EventController {
       model.addAttribute("events", EventData.getAll());
       return "events/index";
     }
+
+//    added another attribute below, an empty Event object to handle the no-arg constructor.
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute("event", new Event());
         return "events/create";
     }
 
     @PostMapping("create") //now because of the @ModelAttribute, when this method is called, Spring will create a new Event object
-    public String processForm(@ModelAttribute Event newEvent){
+    public String processForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
+
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg", "Bad data!");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:/events"; //this takes it back to the display events page
     }
