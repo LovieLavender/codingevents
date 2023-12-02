aeb9ddafb803c8a2d1f4aef8f42d11f3a5a9df86
Lavender <lavendersanders88@yahoo.com>
2023-12-01 23:53:20 -0600

    one-to-one

--- codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
+++ codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,7 +1,7 @@
 package org.launchcode.codingevents.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -18,23 +18,22 @@ public class Event extends AbstractEntity{
     @NotBlank(message = "Name is required.")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
-    @Size(max = 500, message = "Description too long!")
-    private String description;
 
-    @NotBlank
-    @Email(message = "Invalid email. Try again.")
-    private String contactEmail;
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    private EventDetails eventDetails;
+    //the above field establishes the relationship between event and event details.
 
     //   private Event type; this is being replaced with the below,so we can set up a relationship
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
 
-    public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
+
+    public Event(String name, EventCategory eventCategory) {
 //        this(); no longer need this either
         this.name = name;
-        this.description = description;
-        this.contactEmail = contactEmail;
         this.eventCategory = eventCategory;
 
     }
@@ -53,21 +52,6 @@ public class Event extends AbstractEntity{
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
 
     public EventCategory getEventCategory() {
         return eventCategory;
@@ -75,6 +59,14 @@ public class Event extends AbstractEntity{
 
     public void setEventCategory(EventCategory eventCategory) {
         this.eventCategory = eventCategory;
+    }
+
+    public EventDetails getEventDetails() {
+        return eventDetails;
+    }
+
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
     }
 
     @Override
---
+++ codingevents/src/main/java/org/launchcode/codingevents/models/EventDetails.java
@@ -0,0 +1,45 @@
+package org.launchcode.codingevents.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class EventDetails extends AbstractEntity {
+
+    @Size(max = 500, message = "Description too long!")
+    private String description;
+
+    @NotBlank
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    //the below field sets up the inverse relationship so event details can know about the event. This application do not need this so this is just for informational purposes.
+//    @OneToOne(mappedBy = "eventDetails")
+//    private Event event;
+
+    public EventDetails(String description, String contactEmail){
+        this.description= description;
+        this.contactEmail= contactEmail;
+    }
+    public EventDetails() {}
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+
+}
